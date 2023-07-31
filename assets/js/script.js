6673979f3ab947bc7b570534428019844ade5871
Jalen Bailey <jwbailey13@gmail.com>
2023-07-31 10:55:52 -0600

    added the GEOCODING API and Key so that the data would be fetched and validated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
+const API_KEY = "326e7676a644e6577149cca778a9a067"; // API key
+
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); //User enters a city name and removes the extra spaces. 
     if(!cityName) return; //Returns if the cityName is empty
+    const GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API key}'
 
-    console.log(cityName)
+    fetch(GEOCODING_API_URL).then(res=> res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("An error has occurred while fetching the coordinates!")
+    });
 }
 searchButton.addEventListener("click", getCityCoordinates)
