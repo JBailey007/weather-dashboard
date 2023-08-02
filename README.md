e72c5d7a1f38bdb92a5909054ad8439bf6326a4f
Jalen Bailey <jwbailey13@gmail.com>
2023-08-02 14:28:48 -0600

    updated the JS code to remove the formatting. Updated the README to hold the description

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # weather-dashboard
+
+## Description
+This webpage was created/edited using HTML and CSS and JavaScript Code. 
+This webpage is a weather application that allows you to find the weather forecast for any city. 
+
+## Installation
+
+You can access this page using the following link. (https://jbailey007.github.io/work-day-scheduler/)
+
+## Usage
+You may select the input box on the top left of the screen by clicking on it. 
+You can enter in the name of a city and return the current weather for that city as well as the next 5 day forecast. 
+
+Once you have searched for a city. You can refresh the page and see all of the previous searches that you have made in your local storage that are displayed below the button. 
+
+You can click on those buttons and it will search again for that location.
+
+# Credits
+N/A
+
+# Screenshot
+![Screenshot of Weather Dashboard Application](<Screenshot 2023-08-02 142603.png>)
+
+## License
+Please refer to the LICENSE in the repo. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,172 +1,118 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
-const currentWeatherDiv = document.querySelector(".current-weather");
+const currentWeatherDiv = document.querySelector(".current-weather")
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const historyDiv = document.querySelector(".history-card");
+const historyDiv = document.querySelector(".history-card")
+
 
 const API_KEY = "326e7676a644e6577149cca778a9a067"; // API key
 
 const createWeatherCard = (cityName, weatherItem, index) => {
-  const kelvinTemp = weatherItem.main.temp;
-  const fahrenheitTemp = ((kelvinTemp - 273.15) * 9) / 5 + 32;
-  const formattedTemp = fahrenheitTemp.toFixed(2);
-  if (index === 0) {
-    //HTML for main weather card
-    return (
-      ' <div class="details"> \
-                    <h2>' +
-      cityName +
-      " (" +
-      weatherItem.dt_txt.split(" ")[0] +
-      ")</h2> \
-                    <h4>Temperature: " +
-      formattedTemp +
-      "°F</h4> \
-                    <h4>Wind: " +
-      (weatherItem.wind.speed * 2.237).toFixed(2) +
-      "MPH</h4> \
-                    <h4>Humidity: " +
-      weatherItem.main.humidity +
-      '%</h4> \
+    const kelvinTemp = weatherItem.main.temp;
+    const fahrenheitTemp = (kelvinTemp - 273.15) * 9/5 + 32;
+    const formattedTemp = fahrenheitTemp.toFixed(2);
+    if (index === 0) { //HTML for main weather card
+        return ' <div class="details"> \
+                    <h2>'+cityName+' ('+weatherItem.dt_txt.split(" ")[0]+')</h2> \
+                    <h4>Temperature: '+formattedTemp+'°F</h4> \
+                    <h4>Wind: '+(weatherItem.wind.speed * 2.237).toFixed(2)+'MPH</h4> \
+                    <h4>Humidity: '+weatherItem.main.humidity+'%</h4> \
                 </div> \
                 <div class="icon"> \
-                    <img src="https://openweathermap.org/img/wn/' +
-      weatherItem.weather[0].icon +
-      '@4x.png" alt="rain cloud with rain below and sun behind"> \
-                    <h4>' +
-      weatherItem.weather[0].description +
-      "</h4> \
-                </div>"
-    );
-  } else {
-    //HTML for the 5 weather cards
-    return (
-      '<li class="card"> \
-                <h3>(' +
-      weatherItem.dt_txt.split(" ")[0] +
-      ')</h3> \
-                <img src="https://openweathermap.org/img/wn/' +
-      weatherItem.weather[0].icon +
-      '@2x.png" alt="rain cloud with rain below and sun behind"> \
-                <h4>Temp: ' +
-      formattedTemp +
-      "°F</h4> \
-                <h4>Wind: " +
-      (weatherItem.wind.speed * 2.237).toFixed(2) +
-      " MPH</h4> \
-                <h4>Humidity: " +
-      weatherItem.main.humidity +
-      "%</h4> \
-            </li>"
-    );
-  }
-};
+                    <img src="https://openweathermap.org/img/wn/'+weatherItem.weather[0].icon+'@4x.png" alt="rain cloud with rain below and sun behind"> \
+                    <h4>'+weatherItem.weather[0].description+'</h4> \
+                </div>';
+    } else { //HTML for the 5 weather cards
+        return '<li class="card"> \
+                <h3>('+weatherItem.dt_txt.split(" ")[0]+')</h3> \
+                <img src="https://openweathermap.org/img/wn/'+weatherItem.weather[0].icon+'@2x.png" alt="rain cloud with rain below and sun behind"> \
+                <h4>Temp: '+formattedTemp+'°F</h4> \
+                <h4>Wind: '+(weatherItem.wind.speed * 2.237).toFixed(2)+' MPH</h4> \
+                <h4>Humidity: '+weatherItem.main.humidity+'%</h4> \
+            </li>';
+    }
+}
 
 const getWeatherDetails = (cityName, lat, lon) => {
-  const WEATHER_API_URL =
-    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&appid=" +
-    API_KEY +
-    "";
+    const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+API_KEY+"";
 
-  fetch(WEATHER_API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      //Filter the forecasts to get only one forecast each day
-      const uniqueForecastDays = [];
-      const fiveDaysForecast = data.list.filter((forecast) => {
-        const forecastDate = new Date(forecast.dt_txt).getDate();
-        if (!uniqueForecastDays.includes(forecastDate)) {
-          return uniqueForecastDays.push(forecastDate);
-        }
-      });
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        //Filter the forecasts to get only one forecast each day
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if(!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
 
-      //clearing the previous weather data
-      cityInput.value = "";
-      weatherCardsDiv.innerHTML = "";
-      currentWeatherDiv.innerHTML = "";
+        //clearing the previous weather data
+        cityInput.value = "";
+        weatherCardsDiv.innerHTML = "";
+        currentWeatherDiv.innerHTML = "";
 
-      //adds the cards to the page and adds to the DOM
-      fiveDaysForecast.forEach((weatherItem, index) => {
-        if (index === 0) {
-          currentWeatherDiv.insertAdjacentHTML(
-            "beforeend",
-            createWeatherCard(cityName, weatherItem, index)
-          );
-        } else {
-          weatherCardsDiv.insertAdjacentHTML(
-            "beforeend",
-            createWeatherCard(cityName, weatherItem, index)
-          );
-        }
-      });
-    })
-    .catch(() => {
-      alert("An error has occurred while fetching the weather forecast!");
+        //adds the cards to the page and adds to the DOM
+        fiveDaysForecast.forEach((weatherItem, index) => { 
+            if(index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            }
+        });
+    }).catch(() => {
+        alert("An error has occurred while fetching the weather forecast!")
     });
-};
+}
 
 const getCityCoordinates = () => {
-  var cityName = cityInput.value.trim(); //User enters a city name and removes the extra spaces.
-  if (!cityName) return; //Returns if the cityName is empty
-  saveCityToLocalStorage(cityName);
+    var cityName = cityInput.value.trim(); //User enters a city name and removes the extra spaces. 
+    if(!cityName) return; //Returns if the cityName is empty
+    saveCityToLocalStorage(cityName);
 
-  const GEOCODING_API_URL =
-    "https://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityName +
-    "&limit=1&appid=" +
-    API_KEY +
-    "";
+    const GEOCODING_API_URL = "https://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid="+API_KEY+"";
 
-  //Get city coordinates from the API response
-  fetch(GEOCODING_API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      if (!data.length)
-        return alert("No coordinates fount for " + cityName + "");
-      const { name, lat, lon } = data[0];
-      getWeatherDetails(name, lat, lon);
-    })
-    .catch(() => {
-      alert("An error has occurred while fetching the coordinates!");
+    //Get city coordinates from the API response
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        if (!data.length) return alert("No coordinates fount for "+cityName+"");
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("An error has occurred while fetching the coordinates!")
     });
-};
+}
 
 const saveCityToLocalStorage = (cityName) => {
-  let history = localStorage.getItem("searchHistory") || "[]";
-  history = JSON.parse(history);
-
-  // Add the cityName to the history array
-  if (!history.includes(cityName)) {
-    history.push(cityName);
-    localStorage.setItem("searchHistory", JSON.stringify(history));
-  }
-};
+    let history = localStorage.getItem("searchHistory") || "[]";
+    history = JSON.parse(history);
+    
+    // Adds the cityName to the history array
+    if (!history.includes(cityName)) {
+        history.push(cityName);
+        localStorage.setItem("searchHistory", JSON.stringify(history));
+    }
+}
 
 const displaySearchHistory = () => {
-  var cityName = cityInput.value.trim();
-  const history = localStorage.getItem("searchHistory");
-  if (history) {
-    const historyList = JSON.parse(history);
-    historyDiv.innerHTML = "" + cityName + "";
+    var cityName = cityInput.value.trim();
+    const history = localStorage.getItem("searchHistory");
+    if (history) {
+        const historyList = JSON.parse(history);
+        historyDiv.innerHTML = ""+cityName+"";
+        
+        // Creates and adds buttons for each city in the history
+        historyList.forEach((city) => {
+            const historyButton = document.createElement('button');
+            historyButton.textContent = city;
+            historyButton.addEventListener('click', () => {
+                cityInput.value = city;
+                getCityCoordinates();
+            });
+            historyDiv.appendChild(historyButton);
+        });
+    }
+}
 
-    // Create and add buttons for each city in the history
-    historyList.forEach((city) => {
-      const historyButton = document.createElement("button");
-      historyButton.textContent = city;
-      historyButton.addEventListener("click", () => {
-        cityInput.value = city;
-        getCityCoordinates();
-      });
-      historyDiv.appendChild(historyButton);
-    });
-  }
-};
-
-searchButton.addEventListener("click", getCityCoordinates);
+searchButton.addEventListener("click", getCityCoordinates)
 
 displaySearchHistory();
